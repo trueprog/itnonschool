10136af804c6b49c05a50914a65a093da881051d
trueprog <trueprogandy@gmail.com>
2017-12-12 23:56:44 +0100

    Sign in code

--- site/config/routes.rb
+++ site/config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
 
+  get    '/signin',   to: 'sessions#new'
+  post   '/signin',   to: 'sessions#create'
+  delete '/signout',  to: 'sessions#destroy'
+
   resources :users
 end
