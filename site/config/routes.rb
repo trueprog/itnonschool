529e2fb60482c8b4bf4e05889b279f165de4b902
trueprog <trueprogandy@gmail.com>
2017-12-12 16:32:37 +0100

    User show page

--- site/config/routes.rb
+++ site/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'sessions/new'
 
   root "pages#show", page: "index"
   get "/pages/:page" => "pages#show"
+
+  get  '/signup',  to: 'users#new'
+
+  resources :users
 end
