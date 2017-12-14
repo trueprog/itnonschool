a2c4771b1b7ec8733663d6155147ae0a8be2df78
trueprog <trueprogandy@gmail.com>
2017-12-14 20:06:11 +0100

    Sign out and test

--- site/test/integration/users_signin_test.rb
+++ site/test/integration/users_signin_test.rb
@@ -16,15 +16,23 @@ class UsersSigninTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test "login with valid information" do
+  test "login with valid information followed by logout" do
     get signin_path
     post signin_path, params: { session: { email: @user.email, password: 'password' } }
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", signin_path, count: 0
     assert_select "a[href=?]", signout_path
     assert_select "a[href=?]", user_path(@user)
+    delete signout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", signin_path
+    assert_select "a[href=?]", signout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
 
 end
