e346764978d6f8c85fb6d0424e0ee3330159a575
trueprog <trueprogandy@gmail.com>
2017-12-14 19:55:38 +0100

    Added a signin integration test.

--- site/test/integration/users_signin_test.rb
+++ site/test/integration/users_signin_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class UsersSigninTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:michael)
+  end
+
   test "login with invalid information" do
     get signin_path
     assert_template 'sessions/new'
@@ -10,4 +15,16 @@ class UsersSigninTest < ActionDispatch::IntegrationTest
     get root_path
     assert flash.empty?
   end
+
+  test "login with valid information" do
+    get signin_path
+    post signin_path, params: { session: { email: @user.email, password: 'password' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select "a[href=?]", signin_path, count: 0
+    assert_select "a[href=?]", signout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
+
 end
